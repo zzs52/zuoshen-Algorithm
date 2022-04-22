008f5bb164aab0ca8b637b66afc7033e3550e52e
zzs52 <1214734414@qq.com>
2022-04-22 16:08:36 +0800

    modify error

--- src/中级提升/class02/Problem04_ParenthesesDeep.java
+++ src/中级提升/class02/Problem04_ParenthesesDeep.java
@@ -54,24 +54,4 @@ public class Problem04_ParenthesesDeep {
         }
         return max;
     }
-
-    public static int maxLength(String str) {
-        if (str == null || str.equals("")) {
-            return 0;
-        }
-        char[] chas = str.toCharArray();
-        int[] dp = new int[chas.length];
-        int pre = 0;
-        int res = 0;
-        for (int i = 1; i < chas.length; i++) {
-            if (chas[i] == ')') {
-                pre = i - dp[i - 1] - 1;
-                if (pre >= 0 && chas[pre] == '(') {
-                    dp[i] = dp[i - 1] + 1 + (pre > 0 ? dp[pre - 1] : 0);
-                }
-            }
-            res = Math.max(res, dp[i]);
-        }
-        return res;
-    }
 }
