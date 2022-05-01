d53b3b3ac2f251ffc184725f78802e53899f0493
zzs52 <1214734414@qq.com>
2022-05-01 21:26:36 +0800

    modify error

--- src/基础入门/class01/Code05_BinarySearch.java
+++ src/基础入门/class01/Code05_BinarySearch.java
@@ -29,7 +29,7 @@ public class Code05_BinarySearch {
         int l = 0;
         int r = arr.length - 1;
         int index = -1;
-        while (l < r) {
+        while (l <= r) {
             int mid = l + ((r - l) >> 1);
             if (arr[mid] >= value) {
                 index = mid;
