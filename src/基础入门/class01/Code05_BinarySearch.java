36631b78ed7dca5f1cb9c90e363400a5c43f4249
zzs52 <1214734414@qq.com>
2022-05-01 22:18:54 +0800

    modify error

--- src/基础入门/class01/Code05_BinarySearch.java
+++ src/基础入门/class01/Code05_BinarySearch.java
@@ -54,16 +54,30 @@ public class Code05_BinarySearch {
         }
         int l = 1;
         int r = arr.length - 2;
-        while (l < r) {
+        return process(arr, l, r);
+    }
+
+    public static int process(int[] arr, int l, int r) {
+        if (l == r && arr[l] < arr[l - 1] && arr[l] < arr[l + 1]) {
+            return l;
+        } else if (l == r - 1) {
+            if (arr[r] < arr[r - 1] && arr[r] < arr[r + 1]) {
+                return r;
+            } else if (arr[l] < arr[l - 1] && arr[l] < arr[l + 1]) {
+                return l;
+            }
+        } else if (l < r - 1) {
             int mid = l + ((r - l) >> 1);
             if (arr[mid] > arr[mid - 1]) {
-                r = mid - 1;
+                return process(arr, l, mid - 1);
             } else if (arr[mid] > arr[mid + 1]) {
-                l = mid + 1;
-            } else {
+                return process(arr, mid + 1, r);
+            } else if (arr[mid] < arr[mid - 1] && arr[mid] < arr[mid + 1]) {
                 return mid;
+            } else {
+                return process(arr, l, mid - 1) != -1 ? process(arr, l, mid - 1) : process(arr, mid + 1, r) == -1 ? -1 : process(arr, mid + 1, r);
             }
         }
-        return l;
+        return -1;
     }
 }
