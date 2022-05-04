10fc0361a8a35d472bf7eeb58fcdbf19082b58a5
zzs52 <1214734414@qq.com>
2022-05-04 21:50:55 +0800

    modify error

--- src/基础提升/class01/Code02_BitMap.java
+++ src/基础提升/class01/Code02_BitMap.java
@@ -13,12 +13,12 @@ public class Code02_BitMap {
         // arr[0] 0-31
         int i = 178; // 想取得178个bit的状态
         int numIndex = 178 / 32;
-        int bitindex = 178 % 31;
+        int bitindex = 178 % 32;
         // 拿到178位的状态
         int s = ((arr[numIndex] >> bitindex) & 1);
         // 请把178位的状态改为1
         arr[numIndex] = arr[numIndex] | (1 << (bitindex));
-        i =178; // 请把178位的状态改为0
+        // 请把178位的状态改为0
         arr[numIndex] = arr[numIndex] &( ~ (1 << bitindex));
     }
 }
